c743cc7cfee2a7dffdf84897e6de17c422d8da82
Donghyeon Kim <kdh0429@snu.ac.kr>
2021-03-08 10:31:31 +0900

    Debug dynamics

---
+++ reproduce_dyros/result/res.m
@@ -0,0 +1,3 @@
+clear all;
+result=load('result.txt');
+plot(result(:,[3,9,15]),'DisplayName','result(:,[3,9,15])')
--- reproduce_dyros/src/reproduce_dyros.cpp
+++ reproduce_dyros/src/reproduce_dyros.cpp
@@ -14,6 +14,7 @@ const int dof = 6;
 typedef Eigen::Matrix< double, 6, 1 > Vector6d;
 typedef Eigen::Matrix< double, 6, 6 > Matrix6d;
 
+// Get C matrix (C*qdot is coriolis+centrifugal torque)
 Eigen::MatrixXd getC(RigidBodyDynamics::Model &model, Vector6d q, Vector6d qdot){
     double h = 2e-12;
     Vector6d q_new;
@@ -66,29 +67,34 @@ int main(int argc, char **argv)
 
     ros::NodeHandle n;
 
+    // Don't use simulation time since we will stop simulation(simulation is used just for rendering)
     ros::param::set("use_sim_time", false);
     ros::Duration(1.0).sleep();
 
     ros::Rate loop_rate(1000);
 
+    // Service client
     ros::ServiceClient physics_client = n.serviceClient<std_srvs::Empty>("/gazebo/pause_physics");
     ros::ServiceClient joint_position_client = n.serviceClient<gazebo_msgs::SetModelConfiguration>("/gazebo/set_model_configuration");
 
+    // Stop simulation
     std_srvs::Empty pauseSrv;
     physics_client.call(pauseSrv);
 
+    // Load URDF model
     RigidBodyDynamics::Model model;
     RigidBodyDynamics::Addons::URDFReadFromFile("/home/kim/doosan_ws/src/doosan-robot/dsr_description/urdf/m0609.urdf", &model, false, false);
 
+    // Load data file
     std::ifstream openFile("/home/kim/ssd2/dusan_ws/2nd/data/robot1_20190829/free/0_00kg/1/DRCL_Data_1.txt");
     std::string line;
     std::stringstream line_ss;
     float data_arr[100];
-
     if( !openFile.is_open() ){
         return 0;
     }
 
+    // Request to render robot configuration
     gazebo_msgs::SetModelConfiguration modelconfig;
     modelconfig.request.model_name = (std::string) "dsr01";
 
@@ -108,28 +114,53 @@ int main(int argc, char **argv)
     modelconfig.request.joint_positions.push_back(0.0);
     modelconfig.request.joint_positions.push_back(0.0);
 
+    // Data and dynamic matrix variables
     Vector6d q;
-    Vector6d q_dot;
+    Vector6d q_dot;    
+    Vector6d tau_m;
+
     Eigen::MatrixXd A;
     Eigen::MatrixXd C;
-    Eigen::VectorXd g;
-    Vector6d q_dot_zero;
-    q_dot_zero.setZero();
+    Eigen::MatrixXd C_T;
+    Eigen::VectorXd g;    
     Eigen::VectorXd nonlinear;
     Vector6d b;
     Vector6d b_custom;
 
+    Vector6d q_dot_zero;
+    q_dot_zero.setZero();
+
+    // Momentum observer variables
+    Vector6d integral_term;
+    integral_term.setZero();
+    Vector6d residual;
+    residual.setZero();
+    Matrix6d K_mob;
+    K_mob.setZero();
+    for (int i=0; i<6; i++)
+    {
+        K_mob(i,i)=10;
+    }
+
     int tick = 0;
-    while (ros::ok())
+    double dt = 0.001;
+    double last_time = 0.0;
+    std::ofstream result; 
+    result.open("/home/kim/doosan_ws/src/doosan-robot/reproduce_dyros/result/result.txt");
+
+    while (ros::ok() && openFile.peek() != EOF)
     {
         int count = 0;
         q.setZero();
         q_dot.setZero();
         A.setZero(6,6);
         C.setZero(6,6);
+        C_T.setZero(6,6);
         g.setZero(6);
         nonlinear.setZero(6);
+        q_dot_zero.setZero();
 
+        // Read data
         float data;
         getline(openFile, line);
         line_ss.str(line);
@@ -140,32 +171,56 @@ int main(int argc, char **argv)
         }
         line_ss.clear();
 
+        // Allocate data
         q << data_arr[7], data_arr[8], data_arr[9], data_arr[10], data_arr[11], data_arr[12];
         q_dot << data_arr[13], data_arr[14], data_arr[15], data_arr[16], data_arr[17], data_arr[18];
+        tau_m << data_arr[1], data_arr[2], data_arr[3], data_arr[4], data_arr[5], data_arr[6];
 
+        // Current dynamics information
         RigidBodyDynamics::CompositeRigidBodyAlgorithm(model, q, A, true);
         C = getC(model, q, q_dot);
-        RigidBodyDynamics::NonlinearEffects(model, q, q_dot, nonlinear);
+        C_T = C.transpose();
+        // q.setZero();
+        // q_dot_zero.setZero();
+        // g.setZero();
         RigidBodyDynamics::NonlinearEffects(model, q, q_dot_zero, g);
-        b = nonlinear - g;
-        b_custom = C * q_dot;
+        // RigidBodyDynamics::NonlinearEffects(model, q, q_dot, nonlinear);
+        // b = nonlinear - g;
+        // b_custom = C * q_dot;
 
-        modelconfig.request.joint_positions[0]= q(0);
-        modelconfig.request.joint_positions[1]= q(1);
-        modelconfig.request.joint_positions[2]= q(2);
-        modelconfig.request.joint_positions[3]= q(3);
-        modelconfig.request.joint_positions[4]= q(4);
-        modelconfig.request.joint_positions[5]= q(5);
+        // Momentum observer
+        dt = data_arr[0] - last_time;
+        if( tick == 0)
+            dt = 0.001;
+        integral_term = integral_term + (tau_m + C_T*q_dot - g - residual)*dt;
+        residual = K_mob * (integral_term - A*q_dot);
+        last_time = data_arr[0];
+        Vector6d t;
+        t = tau_m + C_T*q_dot - g ;
 
-        if (!joint_position_client.call(modelconfig)) ROS_ERROR("Failed to call the service");
+        // Send request to render
+        // modelconfig.request.joint_positions[0]= q(0);
+        // modelconfig.request.joint_positions[1]= q(1);
+        // modelconfig.request.joint_positions[2]= q(2);
+        // modelconfig.request.joint_positions[3]= q(3);
+        // modelconfig.request.joint_positions[4]= q(4);
+        // modelconfig.request.joint_positions[5]= q(5);
+        // if (!joint_position_client.call(modelconfig)) ROS_ERROR("Failed to call the service");
 
         if (tick%100 == 0)
+        {
             std::cout << "Time: " << data_arr[0] << std::endl;
- 
-        loop_rate.sleep();
+            std::cout << "tau_m: " << tau_m(1) << std::endl;
+            std::cout << "g: "  << g(1) << std::endl<< std::endl;
+        }
+        // loop_rate.sleep();
         tick++;
+        result << data_arr[0] << "\t" << residual(0) << "\t" <<residual(1)<< "\t" << residual(2)<< "\t" << residual(3)<< "\t" << residual(4)<< "\t" << residual(5) \
+        << "\t" << data_arr[49]<< "\t" << data_arr[50]<< "\t" << data_arr[51]<< "\t" << data_arr[52]<< "\t" << data_arr[53]<< "\t" << data_arr[54] \
+        << "\t" << tau_m(0)-data_arr[67]<< "\t" << tau_m(1)-data_arr[68]<< "\t" << tau_m(2)-data_arr[69]<< "\t" << tau_m(3)-data_arr[70]<< "\t" << tau_m(4)-data_arr[71]<< "\t" << tau_m(5)-data_arr[72]<< std::endl;
     }
     openFile.close();
+    result.close();
 
     return 0;
 }
